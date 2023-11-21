c3843d51f9ad755ef05d5c68b486dc3b90c75cfb
sebprt <sebastien@kiboko.fr>
2023-11-21 14:13:50 +0100

    Updated the pipeline runner used for tests

--- tests/functional/PipelineRunner.php
+++ tests/functional/PipelineRunner.php
@@ -7,20 +7,18 @@ namespace functional\Kiboko\Component\Flow\SQL;
 use Kiboko\Contract\Bucket\AcceptanceResultBucketInterface;
 use Kiboko\Contract\Bucket\RejectionResultBucketInterface;
 use Kiboko\Contract\Pipeline\PipelineRunnerInterface;
-use Kiboko\Contract\Pipeline\RejectionInterface;
-use Kiboko\Contract\Pipeline\StateInterface;
+use Kiboko\Contract\Pipeline\StepRejectionInterface;
+use Kiboko\Contract\Pipeline\StepStateInterface;
 
 final class PipelineRunner implements PipelineRunnerInterface
 {
     public function run(
         \Iterator $source,
         \Generator $async,
-        RejectionInterface $rejection,
-        StateInterface $state,
-    ): \Iterator {
-        $state->initialize();
-        $rejection->initialize();
-
+        StepRejectionInterface $rejection,
+        StepStateInterface $state,
+    ): \Iterator
+    {
         $source->rewind();
         $async->rewind();
 
@@ -40,8 +38,5 @@ final class PipelineRunner implements PipelineRunnerInterface
 
             $source->next();
         }
-
-        $rejection->teardown();
-        $state->teardown();
     }
 }
